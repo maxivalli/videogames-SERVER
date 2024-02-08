f8aaf48cb4e3a3486ab5db2577ae394900a65cc9
Maximiliano Valli <127133282+maxivalli@users.noreply.github.com>
2024-02-08 01:22:38 -0300

    force false

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const { PORT } = process.env
 
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   // Una vez que la sincronización se haya completado (y se haya forzado la recreación de tablas si es necesario), iniciar el servidor.
   server.listen(3001, () => {
     console.log(`Server listening at PORT: ${PORT}`); 
